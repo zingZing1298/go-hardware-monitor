dc84115f582a9e4d42b330116ff6544a60c99971
zingZing1298 <maheshvarma691@gmail.com>
2024-07-20 16:19:23 -0400

    feat: added network sent and recieved byte logging

--- main.go
+++ main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 	"time"
 
 	"github.com/shirou/gopsutil/v3/cpu"
 	"github.com/shirou/gopsutil/v3/disk"
 	"github.com/shirou/gopsutil/v3/mem"
+	"github.com/shirou/gopsutil/v3/net"
 )
 
 type Metrics struct {
 	CPUUsage    float64
 	MemoryUsage float64
 	DiskUsage   float64
+	NetworkSent uint64
+	NetworkRecv	uint64
 }
 
 func getMetrics() Metrics {
@@ -26,10 +30,15 @@ func getMetrics() Metrics {
 	// Get disk usage
 	diskUsage, _ := disk.Usage("/")
 
+	//get network Usage
+	netStats,_ := net.IOCounters(false)
 	return Metrics{
 		CPUUsage:    cpuPercent[0],
 		MemoryUsage: vmStat.UsedPercent,
 		DiskUsage:   diskUsage.UsedPercent,
+		NetworkSent: netStats[0].BytesSent,
+		NetworkRecv: netStats[0].BytesRecv,
+
 	}
 }
 
@@ -39,7 +48,16 @@ func metricsHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, metrics)
 }
 
+func redirectHandler(w http.ResponseWriter, r *http.Request){
+	http.Redirect(w,r, "/metrics",http.StatusMovedPermanently)
+
+}
+
+
 func main() {
+	http.HandleFunc("/", redirectHandler)
 	http.HandleFunc("/metrics", metricsHandler)
-	http.ListenAndServe(":8000", nil)
+	if err := http.ListenAndServe(":8000", nil); err != nil{
+		log.Fatalf("Could not start server:  %s \n", err)
+	}
 }
